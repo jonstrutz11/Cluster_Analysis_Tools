82cf930308bc53183a6cc765c418cf0f03987914
Jon Strutz <jonathanstrutz2021@u.northwestern.edu>
2018-01-31 15:58:10 -0600

    Added support for user to choose output filepath

--- sequence_cleaner.py
+++ sequence_cleaner.py
@@ -9,12 +9,13 @@ import sys
 from Bio import SeqIO
 
 
-def sequence_cleaner(fasta_file, min_length=0.0, por_n=100.0):
+def sequence_cleaner(fasta_infile, fasta_outfile=None, min_length=0.0,
+                     por_n=100.0):
     # Create our hash table to add the sequences
     sequences = {}
 
     # Using the Biopython fasta parse we can read our fasta input
-    for seq_record in SeqIO.parse(fasta_file, "fasta"):
+    for seq_record in SeqIO.parse(fasta_infile, "fasta"):
         # Take the current sequence
         sequence = str(seq_record.seq).upper()
         # Check if the current sequence is according to the user parameters
@@ -33,13 +34,14 @@ def sequence_cleaner(fasta_file, min_length=0.0, por_n=100.0):
                 sequences[sequence] += "_" + seq_record.id
 
     # Write the clean sequences
-    # Create a file in the same directory where you ran this script
-    with open("clear_" + fasta_file, "w+") as output_file:
-        # Just read the hash table and write on the file as a fasta format
+    if not fasta_outfile:
+        fasta_outfile = "clear_" + fasta_infile
+    with open(fasta_outfile, "w+") as output_file:
+        # Just read the hash table and write the file in fasta format
         for sequence in sequences:
             output_file.write(">" + sequences[sequence] + "\n" + sequence + "\n")
 
-    print("CLEAN!!!\nPlease check clear_" + fasta_file)
+    print("CLEAN!!!\nPlease check clear_" + fasta_infile)
 
 
 userParameters = sys.argv[1:]
@@ -48,11 +50,14 @@ try:
     if len(userParameters) == 1:
         sequence_cleaner(userParameters[0])
     elif len(userParameters) == 2:
-        sequence_cleaner(userParameters[0], float(userParameters[1]))
+        sequence_cleaner(userParameters[0], userParameters[1])
     elif len(userParameters) == 3:
-        sequence_cleaner(userParameters[0], float(userParameters[1]),
-                         float(userParameters[2]))
+        sequence_cleaner(userParameters[0], userParameters[1], float(
+            userParameters[2]))
+    elif len(userParameters) == 4:
+        sequence_cleaner(userParameters[0], userParameters[1], float(
+            userParameters[3]), float(userParameters[3]))
     else:
-        print("There is a problem!")
+        print("Please provide an input fasta file")
 except TypeError:
     print("There is a problem!")
